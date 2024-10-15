426ba1a5bb415e0a9515f4256463340e7ce05994
flavius <flaviuscojocaru19@gmail.com>
2024-10-15 18:56:41 +0300

    Lab2 tasklist mockup

--- Lab_2/main.js
+++ Lab_2/main.js
@@ -21,6 +21,50 @@ const tasks = [
     }
 ]
 
-const printTasks = () => {
-    
+const printTasks = (tasks) => {
+    tasks.forEach(element => {
+        console.log(`Task ${element.id}: ${element.description}
+${element.responsible} is responsible for this task
+------------------------------------`)
+    })
 }
+
+printTasks(tasks);
+
+const displayTasks = (tasks) => {
+
+    const taskContainer = document.getElementById('task-container');
+    
+    if (tasks.length === 0) {
+        const noTasksWarning = document.createElement('p');
+        noTasksWarning.textContent = 'No tasks available';
+        taskContainer.appendChild(noTasksWarning);
+    } else {
+        const tasksList = document.createElement('ul');
+        tasks.forEach(task => {
+            const taskItem = document.createElement('li');
+            const taskId = document.createElement('div');
+            taskId.textContent = task.id;
+            taskItem.appendChild(taskId);
+            
+            const taskResponsible = document.createElement('div');
+            taskResponsible.textContent = task.responsible;
+            taskItem.appendChild(taskResponsible);
+
+            const taskDescription = document.createElement('div');
+            taskDescription.textContent = task.description;
+            taskItem.appendChild(taskDescription);
+
+            const taskStatus = document.createElement('div');
+            taskStatus.textContent = task.status;
+            taskItem.appendChild(taskStatus);
+
+            
+            tasksList.appendChild(taskItem);
+        })
+        taskContainer.appendChild(tasksList);x
+    }
+
+}
+
+displayTasks(tasks)
