68bb5eb2c2423d0d1db5a13a4d95e51a3a1cbe18
flavius <flaviuscojocaru19@gmail.com>
2024-10-15 18:29:35 +0300

    Lab2 in progress

---
+++ Lab_2/main.js
@@ -0,0 +1,26 @@
+console.log("I'm in")
+
+const tasks = [
+    {
+        'id':1,
+        'responsible': 'Mihai',
+        'description': 'do the dishes',
+        'status': 'pending'
+    },
+    {
+        'id':2,
+        'responsible': 'Elena',
+        'description': 'do the homework',
+        'status': 'done'
+    },
+    {
+        'id':3,
+        'responsible': 'Cosmin',
+        'description': 'buy electric castle tickets',
+        'status': 'in progress'
+    }
+]
+
+const printTasks = () => {
+    
+}
