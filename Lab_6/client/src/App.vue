41b99b34cae4789bc9aec532275f57066fc6dc8a
flavius <flaviuscojocaru19@gmail.com>
2024-11-13 10:47:27 +0200

    lab 6 - kind of a crash course

---
+++ Lab_6/client/src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <v-app>
+    <v-main>
+      <Navigation />
+      <router-view/>
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+import Navigation from './components/Navigation.vue';
+export default {
+
+  components: {
+    Navigation,
+  },
+
+  name: 'App',
+
+  data: () => ({
+    //
+  }),
+}
+</script>
---
+++ Lab_6/client/src/components/Navigation.vue
@@ -0,0 +1,21 @@
+<template>
+    <v-app-bar :elevation="2">
+        <template v-slot:prepend>
+    <v-app-bar-nav-icon></v-app-bar-nav-icon>
+    </template>
+
+  <v-app-bar-title>Application Bar</v-app-bar-title>
+  <router-link to="/">Home</router-link> |
+    <router-link to="/about">About</router-link> |
+    <router-link to="/register">Register</router-link>
+</v-app-bar>
+</template>
+
+<script>
+
+export default {
+  name: 'Navigation',
+
+}
+
+</script>
